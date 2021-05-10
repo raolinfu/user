719adcb3a65bdb17009b832a9c9a63d4de4f8ddf
unknown <1145333729@qq.com>
2021-05-10 23:03:43 +0800

    添加视图

--- src/main/java/com/rao/demo/controller/UserController.java
+++ src/main/java/com/rao/demo/controller/UserController.java
@@ -1,19 +1,30 @@
 package com.rao.demo.controller;
 
 import com.rao.demo.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.jws.WebParam;
 
-@RestController
+@Controller
 public class UserController {
     @Resource
     private UserService userService;
 
-    @GetMapping(value = "user")
+    @GetMapping(value = "/user")
+    @ResponseBody
     public String getUser(Integer id){
         return userService.gerUser(id).toString();
     }
 
+    @GetMapping(value = "/login")
+    public String getLogin(Model model){
+        model.addAttribute("name", "rao");
+        model.addAttribute("lin", "hu");
+        return "login";
+    }
 }
--- src/test/java/com/rao/demo/DemoApplicationTests.java
+++ src/test/java/com/rao/demo/DemoApplicationTests.java
@@ -3,26 +3,10 @@ package com.rao.demo;
 import com.rao.demo.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
-
-import javax.sql.DataSource;
 
 @SpringBootTest
 class DemoApplicationTests {
-    @Value("${spring.profiles.active}")
-    private String env;
-
-    @Value("${server.port}")
-    private Integer port;
-
-    @Value("${person.name}")
-    private String name;
-
-    @Autowired
-    private DataSource dataSource;
-
     @Autowired
     private UserMapper userMapper;
 
